5f3c57d79dd70f59905057bbde21177c1bd12416
Jonathan Galvao Diniz <jo.diniz@live.fr>
2019-04-15 14:48:39 +0200

    adding cardHero css in jsx

--- src/components/CardHero/CardHero.js
+++ src/components/CardHero/CardHero.js
@@ -1,13 +1,29 @@
 import React from 'react';
-import './CardHero.css';
 import CardFrame from './CardFrame/CardFrame';
 import CardHead from './CardHead/CardHead';
 import CardBackground from './CardBackground/CardBackground';
 import CardLife from './CardLife.jsx';
-//creation de la card et envoie des données via les props
+
 const CardHero = ({ props }) => {
+	const cardContainer = {
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+		margin: 'auto',
+		width: '100%',
+		border: '1px solid black',
+		borderRadius: '2vw',
+		background: '#171314',
+		boxSizing: 'border-box',
+		boxShadow: '-8px 9px 16px -3px gray'
+	};
+
+	const img = {
+		width: '100%'
+	};
+
 	return (
-		<div className='card-container'>
+		<div style={cardContainer}>
 			<CardBackground image={props.image} />
 			<CardHead name={props.name} star={props.star} />
 			<CardLife powerstats={props.powerstats} />
