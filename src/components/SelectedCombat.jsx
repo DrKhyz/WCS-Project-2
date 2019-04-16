480f6abf7ef329b152c26cbbf883482178538028
Jonathan Galvao Diniz <jo.diniz@live.fr>
2019-04-16 15:22:50 +0200

    api and search input linked

--- src/components/SelectedCombat.jsx
+++ src/components/SelectedCombat.jsx
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
 import { Row, Col, Button } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
+import CardHero from './CardHero/CardHero';
+import Loading from './Loading.jsx';
 
 class RandomCombat extends Component {
 	state = {
-		search: ''
+		search: '',
+		hero0: { loading: true }
 	};
 
 	handleChange = e => {
 		this.setState({ search: e.target.value });
+	};
+
+	getHerosData = e => {
+		fetch(`https://www.superheroapi.com/api.php/10219454314208202/search/${this.state.search}`)
+			.then(res => res.json())
+			.then(data => data.results.map((h, i) => this.setState({ ['hero' + i]: h, loading: false })));
+		e.preventDefault();
+	};
+
+	loadingHeroes = hero => {
+		return hero.loading ? (
+			<Loading />
+		) : (
+			<div className='animate'>
+				<CardHero props={hero} />
+			</div>
+		);
 	};
 
 	render() {
@@ -23,7 +43,12 @@ class RandomCombat extends Component {
 					<h1>{this.state.search}</h1>
 				</Row>
 				<Row>
-					<input type='text' onChange={this.handleChange} value={this.state.search} name='search' id='search' />
+					<form onSubmit={this.getHerosData}>
+						<label>Name:</label>
+						<input type='text' onChange={this.handleChange} value={this.state.search} name='search' id='search' />
+						<input type='submit' value='Submit' />
+					</form>
+					{this.loadingHeroes(this.state.hero0)}{' '}
 				</Row>
 			</div>
 		);
