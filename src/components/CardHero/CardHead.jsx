607029e52e63d207f582eafb57dd76e72f929cbd
Jonathan Galvao Diniz <jo.diniz@live.fr>
2019-04-22 13:20:38 +0200

    new indent

--- src/components/CardHero/CardHead.jsx
+++ src/components/CardHero/CardHead.jsx
@@ -1,66 +1,66 @@
-import React from 'react';
+import React from "react";
 
 const CardHead = props => {
 	let starsType = [];
-	const filledStar = 'https://img.icons8.com/color/48/000000/filled-star.png';
-	const emptyStar = 'https://img.icons8.com/color/48/000000/star.png';
+	const filledStar = "https://img.icons8.com/color/48/000000/filled-star.png";
+	const emptyStar = "https://img.icons8.com/color/48/000000/star.png";
 	for (let i = 1; i <= 5; i++) {
 		props.star < i ? starsType.push(emptyStar) : starsType.push(filledStar);
 	}
 
 	const cardHead = {
-		width: '94%',
-		fontSize: '0.8vw',
-		position: 'absolute'
+		width: "94%",
+		fontSize: "0.8vw",
+		position: "absolute",
 	};
 
 	const cardName = {
-		position: 'absolute',
-		display: 'flex',
-		alignItems: 'align-items-center',
-		fontSize: '1.5vw',
-		paddingLeft: '3%',
-		fontFamily: 'HeroesFont',
-		width: '40%',
-		marginLeft: '-3%',
-		color: 'white',
-		backgroundColor: 'black',
-		border: '2px solid black',
-		marginTop: '10%',
-		opacity: '0.9',
-		borderTopRightRadius: '10px',
-		borderBottomRightRadius: '10px'
+		position: "absolute",
+		display: "flex",
+		alignItems: "align-items-center",
+		fontSize: "1.5vw",
+		paddingLeft: "3%",
+		fontFamily: "HeroesFont",
+		width: "40%",
+		marginLeft: "-3%",
+		color: "white",
+		backgroundColor: "black",
+		border: "2px solid black",
+		marginTop: "10%",
+		opacity: "0.9",
+		borderTopRightRadius: "10px",
+		borderBottomRightRadius: "10px",
 	};
 
 	const cardLevel = {
-		display: 'flex',
-		position: 'absolute',
-		color: 'white',
-		width: '40%',
-		marginLeft: '62%',
-		backgroundColor: 'black',
-		border: '2px solid black',
-		opacity: '0.9',
-		borderTopLeftRadius: '10px',
-		borderBottomLeftRadius: '10px',
-		marginTop: '10%'
+		display: "flex",
+		position: "absolute",
+		color: "white",
+		width: "40%",
+		marginLeft: "62%",
+		backgroundColor: "black",
+		border: "2px solid black",
+		opacity: "0.9",
+		borderTopLeftRadius: "10px",
+		borderBottomLeftRadius: "10px",
+		marginTop: "10%",
 	};
 
 	const star = {
-		maxHeight: '20%',
-		height: '20%',
-		maxWidth: '20%',
-		width: '20%'
+		maxHeight: "20%",
+		height: "20%",
+		maxWidth: "20%",
+		width: "20%",
 	};
 	return (
 		<div style={cardHead}>
 			<div style={cardName}>{props.name}</div>
 			<div style={cardLevel}>
-				<img src={starsType[0]} alt='star' style={star} />
-				<img src={starsType[1]} alt='star' style={star} />
-				<img src={starsType[2]} alt='star' style={star} />
-				<img src={starsType[3]} alt='star' style={star} />
-				<img src={starsType[4]} alt='star' style={star} />
+				<img src={starsType[0]} alt="star" style={star} />
+				<img src={starsType[1]} alt="star" style={star} />
+				<img src={starsType[2]} alt="star" style={star} />
+				<img src={starsType[3]} alt="star" style={star} />
+				<img src={starsType[4]} alt="star" style={star} />
 			</div>
 		</div>
 	);
--- src/components/CardHero/CardHero.js
+++ src/components/CardHero/CardHero.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import CardFrame from './CardFrame.jsx';
-import CardHead from './CardHead.jsx';
-import CardBackground from './CardBackground/CardBackground';
-import CardLife from './CardLife.jsx';
+import React from "react";
+import CardFrame from "./CardFrame.jsx";
+import CardHead from "./CardHead.jsx";
+import CardBackground from "./CardBackground/CardBackground";
+import CardLife from "./CardLife.jsx";
 
 const CardHero = ({ props }) => {
 	const cardContainer = {
-		display: 'flex',
-		flexDirection: 'column',
-		alignItems: 'center',
-		margin: '0',
-		width: '100%',
-		border: '1px solid black',
-		borderRadius: '2vw',
-		background: '#171314',
-		boxSizing: 'border-box',
-		padding: '0'
+		display: "flex",
+		flexDirection: "column",
+		alignItems: "center",
+		margin: "0",
+		width: "100%",
+		border: "1px solid black",
+		borderRadius: "2vw",
+		background: "#171314",
+		boxSizing: "border-box",
+		padding: "0",
 	};
 
 	return (
@@ -23,7 +23,11 @@ const CardHero = ({ props }) => {
 			<CardBackground image={props.image} />
 			<CardHead name={props.name} star={props.star} />
 			<CardLife powerstats={props.powerstats} />
-			<CardFrame powerstats={props.powerstats} biography={props.biography} appearance={props.appearance} />
+			<CardFrame
+				powerstats={props.powerstats}
+				biography={props.biography}
+				appearance={props.appearance}
+			/>
 		</div>
 	);
 };
