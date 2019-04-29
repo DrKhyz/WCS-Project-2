957077d26c3a64634082c5aad440eff74d427252
Jonathan Galvao Diniz <jo.diniz@live.fr>
2019-04-29 12:42:27 +0200

    fixing lifebar who move in fight

--- src/components/CardHero/CardLife.jsx
+++ src/components/CardHero/CardLife.jsx
@@ -1,34 +1,29 @@
-import React from 'react'
-import { Progress } from 'reactstrap'
-import PropTypes from 'prop-types'
+import React from 'react';
+import { Progress } from 'reactstrap';
+import PropTypes from 'prop-types';
 
 const CardLife = ({ powerstats }) => {
 	const cardLife = {
 		position: 'absolute',
 		marginTop: '5vw',
-		marginLeft: '2vw',
-		width: '100%',
-	}
+		marginLeft: '-9vw',
+		width: '10vw',
+	};
 	const lifeBar = {
 		minHeight: '2px',
 		maxHeight: '0.2vh',
 		width: '10vw',
-	}
+	};
 
 	return (
 		<div style={cardLife}>
-			<Progress
-				style={lifeBar}
-				color='success'
-				value={powerstats.life}
-				max={powerstats.durability}
-			/>
+			<Progress style={lifeBar} color='success' value={powerstats.life} max={powerstats.durability} />
 		</div>
-	)
-}
+	);
+};
 
-export default CardLife
+export default CardLife;
 
 CardLife.propTypes = {
 	powerstats: PropTypes.object.isRequired,
-}
+};
